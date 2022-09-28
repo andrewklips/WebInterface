2349697a109ddc6e31253cf32e9753faee95e5da
andrewklips <andrew.klips@gmail.com>
2022-09-28 19:21:17 +0500

    test upd

--- src/test/java/ru/netology/TestOrderCard.java
+++ src/test/java/ru/netology/TestOrderCard.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -32,17 +33,15 @@ public class TestOrderCard {
     }
 
     @Test
-    public void shouldTest() {
+    void shouldTestValidData() {
 
-        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Мария Кислицина");
-        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79030248197");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__content")).click();
-
-        String actual = driver.findElement(By.className("paragraph")).getText().trim();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-
-        Assertions.assertEquals(expected, actual);
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79998887776");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
     }
 
