9312ebe9312635846137794f9113b14543d6f7b5
andrewklips <andrew.klips@gmail.com>
2022-09-28 19:00:18 +0500

    test upd

--- src/test/java/ru/netology/TestOrderCard.java
+++ src/test/java/ru/netology/TestOrderCard.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -35,17 +35,17 @@ public class TestOrderCard {
     }
 
     @Test
-    void Test() {
+    void shouldTestCorrect() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Иван Петров");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79998887766");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
 
-        String actual = driver.findElement(By.className("paragraph")).getText().trim();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
-        Assertions.assertEquals(expected, actual);
+
     }
 
 }
